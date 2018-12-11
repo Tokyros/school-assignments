79abbd7c6f9b790ac6c9e025569640edabc8fa10
ShaharR <shaharr@wix.com>
2018-12-11 20:48:15 +0200

    HW_2 initial commit

---
+++ OOD/HW_2/src/BaseAddressBookPane.java
@@ -0,0 +1,15 @@
+import java.io.RandomAccessFile;
+
+public class BaseAddressBookPane implements AddressBookPaneIFC {
+    private RandomAccessFile addressBookFile;
+
+
+    public BaseAddressBookPane() {
+
+    }
+
+    @Override
+    public void addButtons() {
+
+    }
+}
