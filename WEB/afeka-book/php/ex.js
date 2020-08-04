aedabc2b18418e6b3ac7652d2117473faae7d53a
Dor256 <dorb@wix.com>
2020-08-04 20:49:20 +0300

    Changes game api

--- WEB/afeka-book/php/ex.js
+++ WEB/afeka-book/php/ex.js
@@ -78,8 +78,7 @@ function setForm(player) {
                     if (!currentPlayer.isPlaying) {
                         return;
                     }
-                    //currentPlayer.name !== playerOne
-                    if (true) {
+                    if (currentPlayer.name !== playerOne) {
                         player.replaceWith(`<h3 class="player-ready">${currentPlayer.name} is Ready</h3>`);
                         if (!playerOne && !playerTwo) {
                             playerOne = currentPlayer.name;
@@ -104,95 +103,10 @@ function setForm(player) {
 setForm($('#player1'));
 setForm($('#player2'));
 
-// Leaderboard?
-if (window.openDatabase) {
-    // db = openDatabase("memory-game", "0.1", "Memory game database", 1024 * 1024);
-    // initializeDatabase().then(initializeGameSetupGUI);
-    initializeGameSetupGUI();
-} else {
-    alert("WebSQL is not supported by your browser!");
-}
+initializeGameSetupGUI();
 
-function initializeGameSetupGUI(gameConfig) {
+function initializeGameSetupGUI() {
     $(".game-setup").css('display', 'flex');
-}
-
-// Leaderboard?
-function persistGameConfig(gameConfig) {
-    db.transaction((t) => {
-        t.executeSql(`UPDATE CONFIG SET gridSize = ${gameConfig.gridSize}, player1 = '${gameConfig.player1}', player2 = '${gameConfig.player2}' WHERE id = 1;`)
-    });
-}
-
-// Leaderboard?
-function addGameResult(gameResult) {
-    db.transaction((t) => {
-        t.executeSql(`INSERT INTO GAMES (player1, player2, winner) VALUES ("${gameResult.player1}", "${gameResult.player2}", "${gameResult.winner}")`)
-    })
-}
-
-// Leaderboard?
-function initializeConfig(t) {
-    return new Promise((res) => {
-        t.executeSql("CREATE TABLE IF NOT EXISTS CONFIG (id, gridSize, player1, player2)");
-        t.executeSql("SELECT * FROM CONFIG", [], (t, config) => {
-            if (!config.rows.length) {
-                t.executeSql('INSERT INTO CONFIG (id, gridSize, player1, player2) VALUES (1, 3, "Player 1", "Player 2")');
-                res({gridSize: 3, player1: "Player 1", player2: "Player 2"});
-            } else {
-                res(config.rows[0])
-            }
-        });
-    })
-}
-
-// Leaderboard?
-function updateLeaderboard(games) {
-    return new Promise((res) => {
-        db.transaction((t) => {
-            t.executeSql("SELECT * FROM GAMES", [], (t, games) => {
-                if (games.rows.length) {
-                    const leaderboard = Array.from(games.rows).reduce((leaderBoard, game) => {
-                        const winner = game.winner;
-                        return {
-                            ...leaderBoard,
-                            [winner]: leaderBoard[winner] ? leaderBoard[winner] + 1 : 1
-                        }
-                    }, {});
-                    const players = Object.keys(leaderboard);
-                    const playersSorted = players.sort((p1, p2) => leaderboard[p2] - leaderboard[p1]);
-                    const playerRows = playersSorted.map((player) => {
-                        return $(`<div class="leaderboard-cell">${player} - ${leaderboard[player]} wins</div>`);
-                    })
-                    $('.leaderboard').empty();
-                    $('.leaderboard').append(`<h1>Leaderboard</h1>`);
-                    $(".leaderboard").append(playerRows);
-                }
-                res();
-            })
-        })
-    })
-}
-
-// Leaderboard?
-function initializeGame(t) {
-    return new Promise((res) => {
-        t.executeSql("CREATE TABLE IF NOT EXISTS GAMES (player1, player2, winner)");
-        updateLeaderboard().then(res);
-    })
-}
-
-// Leaderboard?
-async function initializeDatabase() {
-    return new Promise((res) => {
-        db.transaction(function (t) {
-            const configPromise = initializeConfig(t);
-            const gamePromise = initializeGame(t);
-            Promise.all([configPromise, gamePromise]).then(([config]) => {
-                res(config);
-            });
-        });
-    })
 }
 
 function shuffle(arr) {
@@ -268,21 +182,23 @@ class Modal {
 }
 
 // SHAHAR ATTEMPT
-// async function delay(time) {
-//     return new Promise((res) => setTimeout(res, time))
-// }
+async function delay(time) {
+    return new Promise((res) => setTimeout(res, time))
+}
 
-// function move (playerCards, warCards, isLastRound, lastCards, playerNum) {
-//     if (playerCards.length > 1) {
-//         warCards.push(playerCards.shift());
-//         if (isLastRound) {
-//             lastCards[playerNum] = warCards[warCards.length - 1];
-//         }
-//     } else if (playerCards.length === 1) {
-//         warCards.push(playerCards.shift());
-//         lastCards[playerNum] = warCards[warCards.length - 1];
-//     }
-// }
+function move(player, warCards, isLastRound, lastCards, playerNum) {
+    if (player.deck.length > 1) {
+        player.playCard();
+        warCards.push(player.chosenCard);
+        if (isLastRound) {
+            lastCards[playerNum] = warCards[warCards.length - 1];
+        }
+    } else if (player.deck.length === 1) {
+        player.playCard();
+        warCards.push(player.chosenCard);
+        lastCards[playerNum] = warCards[warCards.length - 1];
+    }
+}
 
 class Game {
     constructor(player1, player2) {
@@ -327,7 +243,7 @@ class War {
         this.modal = new Modal();
     }
 
-    onCardPlayed = (player) => {
+    onCardPlayed = async (player) => {
         if ((this.player1.chosenCard && this.player2.chosenCard) || this.winner) {
             return;
         }
@@ -351,123 +267,64 @@ class War {
                     this.player1.deck = shuffle(this.player1.deck);
                 }));
             } else {
-                setTimeout(() => {
-                    // SHAHAR ATTEMPT
-                    // this.warCards = [this.player1.chosenCard, this.player2.chosenCard];
-                    this.playWar([this.player1.chosenCard, this.player2.chosenCard]);
-                }, 300);
+                await delay(300);
+                this.warCards = [this.player1.chosenCard, this.player2.chosenCard];
+                this.playWar([this.player1.chosenCard, this.player2.chosenCard]);
                 return;
             }
-            setTimeout(() => {
-                this.clearFields();
-                addCardPromise.then((addCards) => {
-                    addCards();
-                    this.player1.chosenCard = null;
-                    this.player2.chosenCard = null;
-                    this.checkForWinner();
-                });
-            }, 700);
+            await delay(700);
+            this.clearFields();
+            const addCards = await addCardPromise;
+            addCards();
+            this.player1.chosenCard = null;
+            this.player2.chosenCard = null;
+            this.checkForWinner();
         }
     }
 
-    async playWar(initialCards) {
-        // SHAHAR ATTEMPT
-        // let lastCards = [];
-        // move(this.player1.deck, this.warCards, false, lastCards, 0);
-        // move(this.player2.deck, this.warCards, false, lastCards, 1);
-        // await delay(300);
-        // $('.pl1').text('X');
-        // $('.pl2').text('X');
-        // await delay(600);
-        // $('.pl1').text('');
-        // $('.pl2').text('');
-        // move(this.player1.deck, this.warCards, false, lastCards, 0);
-        // move(this.player2.deck, this.warCards, false, lastCards, 1);
-        // await delay(300);
-        // $('.pl1').text('X');
-        // $('.pl2').text('X');
-        // await delay(600);
-        // $('.pl1').text('');
-        // $('.pl2').text('');
-        // move(this.player1.deck, this.warCards, false, lastCards, 0);
-        // move(this.player2.deck, this.warCards, false, lastCards, 1);
-        // await delay(300);
-        // $('.pl1').text('X');
-        // $('.pl2').text('X');
-        // $('.pl1').text(`${lastCards[0]}`);
-        // $('.pl2').text(`${lastCards[1]}`);
+    async playWar() {
+        let lastCards = [];
+        move(this.player1, this.warCards, false, lastCards, 0);
+        move(this.player2, this.warCards, false, lastCards, 1);
+        await delay(300);
+        $('.pl1').text('X');
+        $('.pl2').text('X');
+        await delay(600);
+        $('.pl1').text('');
+        $('.pl2').text('');
+        move(this.player1, this.warCards, false, lastCards, 0);
+        move(this.player2, this.warCards, false, lastCards, 1);
+        await delay(300);
+        $('.pl1').text('X');
+        $('.pl2').text('X');
+        await delay(600);
+        $('.pl1').text('');
+        $('.pl2').text('');
+        move(this.player1, this.warCards, true, lastCards, 0);
+        move(this.player2, this.warCards, true, lastCards, 1);
+        await delay(300);
+        $('.pl1').text('X');
+        $('.pl2').text('X');
+        $('.pl1').text(`${lastCards[0]}`);
+        $('.pl2').text(`${lastCards[1]}`);
+        await delay(300);
+        $('.pl1').text('');
+        $('.pl2').text('');
 
-        // if (lastCards[0] > lastCards[1]) {
-        //     this.player1.addCards(this.warCards);
-        // } else if (lastCards[0] === lastCards[1]) {
-        //     if (this.player1.deck.length || this.player2.deck.length) {
-        //         this.playWar();
-        //     } else {
-        //         this.declareTie();
-        //     }
-        // } else {
-        //     this.player2.addCards(this.warCards);
-        // }
-
-        if (this.winner) {
-            return;
-        }
-        let i = 0;
-        let prevPlayer1Card = initialCards[0];
-        let prevPlayer2Card = initialCards[1];
-        this.warCards.push(...initialCards);
-        this.warInterval = setInterval(() => {
-            this.player1.deck.length > 0 ? $('.pl1').text('X') : $('.pl1').text(`${this.player1.chosenCard ? this.player1.chosenCard : prevPlayer1Card}`);
-            this.player2.deck.length > 0 ? $('.pl2').text('X') : $('.pl2').text(`${this.player2.chosenCard ? this.player2.chosenCard : prevPlayer2Card}`);
-            this.player1.deck.length > 0 && this.player1.playCard();
-            this.player2.deck.length > 0 && this.player2.playCard();
-
-            if (this.player1.deck.length <= 0 && this.player2.deck.length > 0) {
-                this.player2.chosenCard && this.warCards.push(this.player2.chosenCard);
-            } else if (this.player2.deck.length <= 0 && this.player1.deck.length > 0){
-                this.player1.chosenCard && this.warCards.push(this.player1.chosenCard);
-            } else if (this.player1.chosenCard && this.player2.chosenCard) {
-                this.warCards.push(this.player1.chosenCard || prevPlayer1Card, this.player2.chosenCard || prevPlayer2Card);
+        if (lastCards[0] > lastCards[1]) {
+            this.player1.addCards(this.warCards);
+        } else if (lastCards[0] === lastCards[1]) {
+            if (this.player1.deck.length || this.player2.deck.length) {
+                this.playWar();
+            } else {
+                this.declareTie();
             }
-            
-            setTimeout(() => {
-                if (i === 2 || (this.player1.deck.length === 0 && this.player2.deck.length === 0)) {
-                    const player1Card = this.player1.chosenCard || prevPlayer1Card;
-                    const player2Card = this.player2.chosenCard || prevPlayer2Card;
-                    $('.pl1').text(`${player1Card}`);
-                    $('.pl2').text(`${player2Card}`);
-                    if (player1Card > player2Card) {
-                        this.player2.addCards(this.warCards);
-                        this.checkForWinner();
-                        this.warCards = [];
-                    } else if (player1Card < player2Card) {
-                        this.player1.addCards(this.warCards);
-                        this.checkForWinner();
-                        this.warCards = [];
-                    } else {
-                        if (this.player1.deck.length === 0 && this.player2.deck.length == 0) {
-                            this.declareTie();
-                        } else {
-                            setTimeout(() => this.playWar([player1Card, player2Card]), 300);
-                        }
-                    }
-                    setTimeout(() => {
-                        this.clearFields();
-                        if (this.player1.deck.length === 0 && this.player2.deck.length === 0 && this.player1.chosenCard === this.player2.chosenCard) {
-                            return;
-                        }
-                        prevPlayer1Card = this.player1.chosenCard ? this.player1.chosenCard : prevPlayer1Card;
-                        prevPlayer2Card = this.player2.chosenCard ? this.player2.chosenCard : prevPlayer2Card;
-                        this.player2.chosenCard = null;
-                        this.player1.chosenCard = null;
-                    }, 700);
-                    clearInterval(this.warInterval);
-                } else {
-                    this.clearFields();
-                }
-                i++;
-            }, 300);
-        }, 600);
+        } else {
+            this.player2.addCards(this.warCards);
+        }
+        this.player1.chosenCard = null;
+        this.player2.chosenCard = null;
+        this.checkForWinner();
     }
 
     clearFields() {
