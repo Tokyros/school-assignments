e626a085e6d92e2d0eeae302af8d158a35474d7c
Dor256 <dorb@wix.com>
2020-10-10 23:31:24 +0300

    Adds diff with arguments

--- LINUX/avc/diff.c
+++ LINUX/avc/diff.c
@@ -1,30 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 int main(int argc, char* argv[]) {
-    FILE* f1 = fopen(argv[1], "r");
-    FILE* f2 = fopen(argv[2], "r");
-
-    if (f1 == NULL || f2 == NULL) {
-        printf("Could not read one of the files\n");
-        exit(1);
-    }
-
+    char *inputFileName;
+    char *outputFileName;
+    int opt;
     char c1;
     char c2;
 
-    while ((c1 = fgetc(f1)) != EOF && (c2 = fgetc(f2)) != EOF) {
-        printf("%c <-> %c\n", c1, c2);
-        if (c1 != c2) {
-            printf("Files do not match\n");
-            exit(0);
+    while ((opt = getopt(argc, argv, "i:o:")) != -1) {
+        switch(opt) {
+            case 'i':
+                inputFileName = optarg;
+                break;
+            case 'o':
+                outputFileName = optarg;
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-i inputFileName] [-o outputFileName]\n", argv[0]);
+                exit(EXIT_FAILURE);
         }
     }
 
-    // TODO make this work
+    FILE* f1 = fopen(inputFileName, "r");
+    FILE* f2 = fopen(outputFileName, "r");
+
+    if (f1 == NULL || f2 == NULL) {
+        printf("Could not read one of the files\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while (c1 != EOF && c2 != EOF) {
+        c1 = fgetc(f1);
+        c2 = fgetc(f2);
+        printf("%c <-> %c\n", c1, c2);
+        if (c1 != c2) {
+            printf("Files do not match\n");
+            exit(EXIT_SUCCESS);
+        }
+    }
+
     if (c1 != EOF || c2 != EOF) {
         printf("Files do not match\n");
-        exit(0);
+        exit(EXIT_SUCCESS);
     }
 
     printf("Files match\n");
